5e4d8b7e96268504c9b45ac350f484633288182e
PsychyBruh <102771797+PsychyBruh@users.noreply.github.com>
2024-09-17 18:36:18 -0400

    fix hopefully

--- public/assets/js/proxy.js
+++ public/assets/js/proxy.js
@@ -16,7 +16,7 @@ aiptag.cmd.player.push(function() {
 		AD_WIDTH: 960,
 		AD_HEIGHT: 540,
 		AD_DISPLAY: 'fullscreen', //default, fullscreen, fill, center, modal-center
-		LOADING_TEXT: 'Loading EMERALD AD',
+		LOADING_TEXT: 'Loading Emerald AD',
 		PREROLL_ELEM: function(){return document.getElementById('videoad')},
 		AIP_COMPLETE: function (state)  {
 			/*******************
@@ -64,7 +64,7 @@ function isUrl(val = "") {
 function resolveURL(url) {
 	switch(localStorage.getItem("shuttle||proxy")) {
 		case "dy": 
-			return "/emerald-dn/" + Ultraviolet.codec.xor.decode(url);
+			return "/shuttle-dn/" + Ultraviolet.codec.xor.decode(url);
 		default:
 		case "uv":
 			return  __uv$config.prefix + __uv$config.encodeUrl(url);
